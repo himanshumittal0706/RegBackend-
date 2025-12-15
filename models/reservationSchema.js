f28579b050466f2c90b9c53903f2f0ed36ac21c0
Himanshu Mittal <himanshumittal0706@gmail.com>
2025-12-15 16:15:37 +0530

    Created Get API

---
+++ models/reservationSchema.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+
+const reservationSchema = new mongoose.Schema({
+
+    firstName: {
+        type: String,
+        required: true,
+        minLength: [3, "First Character Must be at least 3 characters long"],
+        maxLength: [30, "First name can not be exceed 30 characters"]
+    },
+    lastName: {
+        type: String,
+        required: true,
+        minLength: [3, "Last Character Must be at least 3 characters long"],
+        maxLength: [30, "Last name can not be exceed 30 characters"]
+    },
+    date: {
+        type: String,
+        required: true,
+    },
+    time: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        validator: [validator.isEmail, "Provide a Valid Email"]
+    },
+    phone: {
+        type: String,
+        required: true,
+        minLength: [10, "Phone Number must be at least 10 characters"],
+        maxLength: [15, "Phone Number can not be exceed 15 characters"]
+    }
+
+})
+
+export default mongoose.model("Reservation", reservationSchema);
+
+
---
+++ server.js
@@ -0,0 +1,11 @@
+import app from './app.js';
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on PORT🚀🚀 ${process.env.PORT}`);
+})
+
+
+
+
+
+
